992a554497f1b6c6f752ab9c97e3d1bd0751fa6f
alinaelahie <alina.elahie@outlook.com>
2025-03-03 12:25:09 -0500

    Update plot_script.R

--- plot_script.R
+++ plot_script.R
@@ -1,7 +1,38 @@
 # Version info: R 4.2.2, Biobase 2.58.0, GEOquery 2.66.0, limma 3.54.0
-################################################################
-#   Data plots for selected GEO samples
+# Install required packages
+install.packages("data.table")
+install.packages("DESeq2")
+install.packages("GEOquery")
+install.packages("umap")
+install.packages("limma")
+install.packages("car")
 
+#load library 
+library(GEOquery)
+
+#========= Load data from GEO============-
+# load the GEO series matrix file
+geo_data_path <- "/Users/alinaelahie/Desktop/GSE270472_series_matrix.txt.gz"
+gse <- getGEO(filename = geo_data_path)
+
+# extract the expression data from the GEO object
+exprs_data <- exprs(gse[[1]])
+
+# check the dimensions of the expression data
+dim(exprs_data)
+
+#======================= Load the Metadata (conditon)=================
+# load the metadata (condition data)
+library(readxl)
+
+metadata_path <- "/Users/alinaelahie/Desktop/GSE270472_HD_KO_NSC.xlsx"
+metadata <- read_excel(metadata_path)
+
+# view the first few rows of the metadata
+head(metadata)
+
+#===================== Preprocess the data================
+#   Data plots for selected GEO samples
 # load counts table from GEO
 urld <- "https://www.ncbi.nlm.nih.gov/geo/download/?format=file&type=rnaseq_counts"
 path <- paste(urld, "acc=GSE270472", "file=GSE270472_raw_counts_GRCh38.p13_NCBI.tsv.gz", sep="&");
@@ -27,3 +58,43 @@ ump <- umap(t(dat), n_neighbors = 5, random_state = 123)
 plot(ump$layout, main="GSE270472 UMAP plot, nbrs =5", xlab="", ylab="", pch=20, cex=1.5)
 library(car)
 pointLabel(ump$layout, labels = rownames(ump$layout), method="SANN", cex=0.6)
+
+#=================== Differential Expression Analysis ==================
+# load DESeq2 package
+library(DESeq2)
+
+col_data <- data.frame(condition = factor(metadata$condition))
+
+# create DESeqDataSet
+dds <- DESeqDataSetFromMatrix(countData = exprs_matrix_filtered,
+                              colData = col_data,
+                              design = ~ condition)
+
+# pre-filter low count genes
+dds <- dds[rowSums(counts(dds)) > 1, ]
+
+# run DESeq2 analysis
+dds <- DESeq(dds)
+
+# results for differential expression analysis
+res <- results(dds)
+
+# view results
+head(res)
+#===========Create Visulizations================
+#volcano plot?
+
+#==========Differential Abundance Analysis ===============
+# install and load gprofiler2
+install.packages("gprofiler2")
+library(gprofiler2)
+
+# Perform gProfiler analysis
+gprofiler_results <- gprofiler(query = rownames(res)[which(res$pvalue < 0.05)], 
+                               organism = "hsapiens")
+
+# View the results
+head(gprofiler_results)
+################################################################
+
+
