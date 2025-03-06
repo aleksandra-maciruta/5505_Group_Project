9b8af6d1b6b8c67c023fb9a6a6dc813370ea9e40
Rachel <rachelyorke2001@gmail.com>
2025-03-06 13:05:41 -0500

    Code to generate samplesheet

--- main.sh
+++ main.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
 
+# Generate samplesheet
+nextflow run nf-core/fetchngs \
+--input data/ids.csv \
+--outdir ./results/fetchngs \
+--nf_core_pipeline rnaseq \
+-profile docker \
+-w work/fetchngs
 
