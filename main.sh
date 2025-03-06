c2e65cde809182351c9f186b508ed896cd2b31c3
Rachel Yorke <rachelyorke2001@gmail.com>
2025-03-06 21:35:25 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Generate samplesheet
+# Download raw reads with fetchngs -- should create samplesheet??
 nextflow run nf-core/fetchngs \
---input data/ids.csv \
---outdir ./results/fetchngs \
---max_cpus 32 --max_memory 128.GB \
---download_method sratools \
---nf_core_pipeline rnaseq \
--profile docker \
--w work/fetchngs
-
+    --input data/ids.csv \
+    --outdir ./results/fetchngs \
+    --max_cpus 32 --max_memory 128.GB \
+    --download_method sratools \
+    --nf_core_pipeline rnaseq \
+    -profile docker \
+    -w work/fetchngs \
